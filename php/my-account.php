360e42b490c60f57da327136f3e3367c3c55affd
chrislom12 <christiaanlombard12@gmail.com>
2022-05-17 20:40:51 +0200

    Added My Account page
    Simple my account page for login, log out and register

---
+++ php/my-account.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="../css/login.css">
+    <link rel="stylesheet" href="../css/headfoot.css">
+    <title></title>                          
+    <script src= "../js/login.js"></script>
+</head>
+
+<body>
+
+    <?php
+        require ("header.php");
+
+        ?>
+
+    
+    <h1 class=headingPage id="heading">My Account</h1>
+    
+    
+    <div id= "mainDiv" class="loginBlock">
+        <div class="subdiv">
+            <a href="register.php"><button id="loginbtn" class="loginbtn" onclick="">Register</button></a>
+            <a href="login.php"><button id="loginbtn" class="loginbtn" onclick="">Log In</button></a>
+
+            <!-- implement log out below -->
+            <a href=""><button id="loginbtn" type="submit" class="loginbtn" onclick="">Log out</button></a>
+        </div>
+        
+    </div>
+    
+      
+    <?php
+        require ("footer.php");
+        ?>
+</body>
+
+</html>
