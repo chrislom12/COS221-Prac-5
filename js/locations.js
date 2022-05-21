2503bd17eb4f4ecc42cf16d9f52d11e87a106e1a
chrislom12 <christiaanlombard12@gmail.com>
2022-05-21 23:42:16 +0200

    Partial implementation of locations
    Added php, js and css for locations (partially)

---
+++ js/locations.js
@@ -0,0 +1,46 @@
+function insertClick() {
+    //changes the input format and button styles to show selected button for insert
+    var btnInsert = document.getElementById("insertBtn");
+    btnInsert.style.background = "#003347";
+    document.getElementById("updateBtn").style.background = "#004e6d";
+    document.getElementById("deleteBtn").style.background = "#004e6d";
+    document.getElementById("dataDiv").innerHTML = '<input type="hidden" id="type" name="type" value="insert"><label for="name" id="nameLabel">Team Name</label><input type="text" placeholder="Enter Team Name" name="name" id="name" class="formInput" required><br><label for="publisher" id="publisherLabel">Publisher ID</label><input type="text" placeholder="Enter Publisher ID" name="publisher" id="publisher" class="formInput" required><br><label for="site" id="siteLabel">Home Site ID</label><input type="text" placeholder="Enter Home Site ID" name="site" id="site" class="formInput" required>';
+}
+
+function updateClick() {
+    //changes the input format and button styles to show selected button
+    var btnInsert = document.getElementById("updateBtn");
+    btnInsert.style.background = "#003347";
+    document.getElementById("insertBtn").style.background = "#004e6d";
+    document.getElementById("deleteBtn").style.background = "#004e6d";
+    document.getElementById("dataDiv").innerHTML = '<input type="hidden" id="type" name="type" value="update"><label for="update" id="updateLabel">What do you need updated?</label><select name="updateNeeded" id="updateNeeded" class = "formInput"><option value="name">Team Name</option><option value="site">Team Home Site ID</option><option value="publisher">Team Publisher ID</option></select><br><label for="team" id="teamLabel">Team ID</label><input type="text" placeholder="Enter the Team ID" name="team" id="team" class="formInput"><br><label for="newVal" id="newValLabel">New Value</label><input type="text" placeholder="Enter the new value" name="newVal" id="newVal" class="formInput" required>';
+}
+
+function deleteClick() {
+    //changes the input format and button styles to show selected button
+    var btnInsert = document.getElementById("deleteBtn");
+    btnInsert.style.background = "#003347";
+    document.getElementById("updateBtn").style.background = "#004e6d";
+    document.getElementById("insertBtn").style.background = "#004e6d";
+    document.getElementById("dataDiv").innerHTML = '<input type="hidden" id="type" name="type" value="delete"><label for="delete" id="deleteLabel">Team ID</label><input type="text" placeholder="Enter the team ID to delete" name="delete" id="delete" class="formInput" required>';
+}
+
+function submitForm() {
+    /* insert validation */
+    const xmlhttp = new XMLHttpRequest();
+    xmlhttp.onload = function () {
+        document.getElementById("table").innerHTML = this.responseText;
+    }
+    xmlhttp.open("GET", "populateTable.php?type=team");
+    xmlhttp.send();
+    document.getElementById("insertForm").submit();
+}
+
+function tableClick() {
+    const xmlhttp = new XMLHttpRequest();
+    xmlhttp.onload = function () {
+        document.getElementById("table").innerHTML = this.responseText;
+    }
+    xmlhttp.open("GET", "populateTable.php?type=team");
+    xmlhttp.send();
+}
---
+++ php/locations.php
@@ -0,0 +1,63 @@
+<!--COS221 PerfectStrangers-->
+<!DOCTYPE html>
+
+<head>
+
+    <link rel="stylesheet" href="../css/locations.css">
+    <link rel="stylesheet" href="../css/headfoot.css">
+    <title>COS221 Prac 5</title>
+    <script src= "../js/locations.js"></script>
+
+</head>
+
+<body onload="insertClick(), tableClick()">
+
+    <?php
+    include 'header.php';
+    ?>
+    <h1 class=headingPage id="heading">Locations</h1>
+    <div class="mainBlock">
+        <div class ="queriesBlock">
+            <div class ="optionsBlock">
+                <button id="insertBtn" class = "choiceQueryBtn" onclick="insertClick()">Add Location</button>
+                <button id="updateBtn" class = "choiceQueryBtn" onclick="updateClick()">Add Address</button>
+                <button id="deleteBtn" class = "choiceQueryBtn" onclick="deleteClick()">Delete Swimmer</button>
+            </div>
+            <div class ="inputBlock">
+                <div class="subdiv">
+                    <form action="swimmers-validate.php" method="post" id="insertForm">
+                        <div class="insertDiv" id = "dataDiv">
+                        <input type="hidden" id="type" name="type" value="update">
+                        <label for="update" id="updateLabel">What do you need updated?</label>
+                        <select name="updateNeeded" id="updateNeeded" class = "formInput">
+                            <option value="name">Name</option>
+                            <option value="surname">Surname</option>
+                            <option value="team">Team ID</option>
+                            <option value="address">Address ID</option>
+                        </select>
+                        <br>
+                        <label for="person" id="personLabel">Person ID</label>
+                        <input type="text" placeholder="Enter the person ID" name="person" id="person" class="formInput">
+                        <br>
+                        <label for="newVal" id="newValLabel">New Value</label>
+                        <input type="text" placeholder="Enter the new value" name="newVal" id="newVal" class="formInput">
+                        </div>     
+                    </form>
+                </div>   
+            </div>
+            <div class ="confirmBlock">
+                <button class = "choiceQueryBtn" id="confirmBtn" onclick="submitForm()">Confirm</button>
+            </div>
+        </div>
+        <div class ="tableBlock" id="table">
+            
+        </div>
+    </div>
+    
+    <?php
+        require ("footer.php");
+    ?>
+
+</body>
+
+</html>
