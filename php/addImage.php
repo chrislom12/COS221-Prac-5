32e40a241eaeb6c36c2df0f5f04e4bf4b07d63ab
chrislom12 <christiaanlombard12@gmail.com>
2022-06-01 19:14:34 +0200

    Removed tournaments from all media
    Tournament media can no longer be uploaded
    - removed file on server
    - removed upload option for tournaments

--- php/addImage.php
+++ php/addImage.php
@@ -54,20 +54,6 @@
             echo "<script>alert('Sorry, there was an error uploading your file.')</script>";
           }
     }
-    else if($imageType=="Tournament"){
-
-  
-        $target_file = $target_dir ."tournaments/".$image['name'];
-
-
-        if (move_uploaded_file($_FILES["img"]["tmp_name"], $target_file)) {
-            /* echo "The file ". htmlspecialchars( basename( $_FILES["img"]["name"])). " has been uploaded."; */
-            echo "<script>alert('The file ". htmlspecialchars( basename( $_FILES["img"]["name"])). " has been uploaded.')</script>";
-          } else {
-            /* echo "Sorry, there was an error uploading your file."; */
-            echo "<script>alert('Sorry, there was an error uploading your file.')</script>";
-          }
-    }
     
     
 ?>
